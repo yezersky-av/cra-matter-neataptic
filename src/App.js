6bb302eb3b77ed645d765b1ec319b7d30eab5709
Andrew Yezersky <yezerskyav@gmail.com>
2017-12-15 17:36:05 +0300

    Player raycasting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import matterJS, {
+//     Engine,
+//     Render,
+//     Runner,
+//     Composite,
+//     Composites,
+//     Common,
+//     Query,
+//     MouseConstraint,
+//     Mouse,
+//     Events,
+//     World,
+//     Vertices,
+//     Bodies,
+//     Vector
+// } from 'matter-js';
 import matter from './matter';
 
 class App extends Component {
@@ -8,8 +24,9 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.canvas = null;
-        this.ctx = null;
+        // this.ctx = null;
         this.matter = null;
+        this.player = null;
     }
 
     getCanvasRef = (canvas) => {
@@ -21,8 +38,21 @@ class App extends Component {
         //     this.ctx = this.canvas.getContext('2d');
         //     this.ctx.fillRect(0, 0, this.props.width, this.props.height);
         // }
-        this.matter = matter(this.props.width, this.props.height);
+        this.matter = matter(this.props.width, this.props.height, {
+            position: {
+                x: 400,
+                y: 100,
+            },
+            radius: 100,
+            direction: {
+                x: 0,
+                y: 0,
+            },
+            rays: 3
+        });
         console.log('matter: ', this.matter);
+        this.player = this.matter.player;
+        // console.log("this.player: ", this.player);
     }
 
     render() {
--- src/matter.js
+++ src/matter.js
@@ -10,32 +10,44 @@ import {
     Mouse,
     Events,
     World,
-    Vertices,
+    Body,
+    // Vertices,
     Bodies,
     Vector
 } from 'matter-js';
 
-function raycast(bodies, start, r, dist) {
-    let normRay = Vector.normalise(r);
+function raycast(bodies, start, direction, dist, filterFunc = () => (true)) {
+    let normRay = Vector.normalise(direction);
     let ray = normRay;
-    let point = Vector.add(ray, start);
+    // let point = Vector.add(ray, start);
     for (let i = 0; i < dist; i++) {
         ray = Vector.mult(normRay, i);
         ray = Vector.add(start, ray);
-        let bod = Query.point(bodies, ray)[0];
+        let bod = Query.point(bodies.filter(filterFunc), ray)[0];
         if (bod) {
             return {point: ray, body: bod};
         }
     }
-    return;
+    return null;
+}
+
+function drawPlayer(player, context) {
+
+    context.beginPath();
+    context.arc(player.position.x, player.position.y, player.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = '#003300';
+    context.stroke();
 }
 
 export default (width, height) => {
+    // let player = null;
     // create engine
     let engine = Engine.create(),
         world = engine.world;
-
-    let testUnits = new Array(30);
+    world.gravity = {x: 0, y: 0};
 
     // create renderer
     let render = Render.create({
@@ -69,16 +81,28 @@ export default (width, height) => {
                 let sides = Math.round(Common.random(1, 8));
                 sides = (sides === 3) ? 4 : sides;
                 return Bodies.polygon(x + ((Math.random() * swing) - swing / 2), y + ((Math.random() * swing) - swing / 2), sides, Common.random(20, 50), {isStatic: true});
+
+            default:
+                break;
         }
     });
 
-    let star = Vertices.fromPath('50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38'),
-        concave = Bodies.fromVertices(200, 200, star);
-
+    let player = Bodies.circle(400, 150, 10, {
+        // isSensor: true,
+        // collisionFilter: {
+        //     category: 0x0011,
+        //     mask: 0xFFFFFFF0,
+        //     group: 1
+        // }
+    });
+    Body.rotate(player, Math.PI / 2);
+    // player.render.strokeStyle = '#FF0000';
+    // player.angle = 200;
     World.add(world, [
         stack,
         // concave,
         // walls
+        player,
         Bodies.rectangle(width / 2, 25 / 2, width, 25, {isStatic: true}),
         Bodies.rectangle(width / 2, height - (25 / 2), width, 25, {isStatic: true}),
         Bodies.rectangle(width - (25 / 2), height / 2, 25, height, {isStatic: true}),
@@ -86,69 +110,94 @@ export default (width, height) => {
     ]);
 
     Events.on(render, 'afterRender', function () {
+        // Body.setAngle(player, Math.random() * Math.PI / 180);
         let mouse = mouseConstraint.mouse,
             context = render.context,
             bodies = Composite.allBodies(engine.world),
-            startPoint = {x: 400, y: 100},
-            endPoint = mouse.position;
+            startPoint = player.position,//{x: 400, y: 100},
+            rayDirection = Vector.rotate({x: 1, y: 0}, player.angle/* * (180 / Math.PI)*/),
+            endPoint = Vector.rotate({x: 1, y: 0}, player.angle /* * (180 / Math.PI)*/);//mouse.position;
+        console.log('endPoint: ', endPoint);
 
-
-        let collisions = raycast(bodies, startPoint, Vector.sub(mouse.position,startPoint), 400);
+        let collisions = [
+            raycast(bodies, startPoint, rayDirection, 400, (item) => {
+                return item !== player;
+            }), raycast(bodies, startPoint, Vector.rotate(rayDirection, 45), 400, (item) => {
+                return item !== player;
+            }), raycast(bodies, startPoint, Vector.rotate(rayDirection, -45), 400, (item) => {
+                return item !== player;
+            })
+        ];
+        console.log('collisions: ', collisions);
         //Query.ray(bodies, startPoint, endPoint);
-        // if (collisions.length) console.log('collisions: ', collisions);
 
         Render.startViewTransform(render);
 
-
-        if (Array.isArray(collisions)) {
-            context.beginPath();
-            context.moveTo(startPoint.x, startPoint.y);
-            context.lineTo(endPoint.x, endPoint.y);
-            if (collisions.length > 0) {
-                context.strokeStyle = '#fff';
-            } else {
-                context.strokeStyle = '#555';
-            }
-            context.lineWidth = 0.5;
-            context.stroke();
-
-            for (let i = 0; i < collisions.length; i++) {
-                let collision = collisions[i];
-                context.rect(collision.bodyA.position.x - 4.5, collision.bodyA.position.y - 4.5, 8, 8);
-                context.rect(
-                    collision.body.position.x - collision.penetration.x - 4,
-                    collision.body.position.y - collision.penetration.y - 4,
-                    8, 8);
-                console.log(`collisions: ${i}`, collision);
-            }
-        }
-        else {
-            if (collisions && collisions.point) {
+        collisions.forEach((collision) => {
+            if (collision && collision.point) {
                 context.beginPath();
                 context.moveTo(startPoint.x, startPoint.y);
-                context.lineTo(collisions.point.x, collisions.point.y);
+                context.lineTo(collision.point.x, collision.point.y);
 
                 // console.log('collisions: ', collisions);
                 context.strokeStyle = '#F88';
                 context.lineWidth = 0.7;
                 context.stroke();
             }
-            else {
-                context.beginPath();
-                context.moveTo(startPoint.x, startPoint.y);
-                context.lineTo(endPoint.x, endPoint.y);
-                context.strokeStyle = '#FFF';
-                context.lineWidth = 0.5;
-                context.stroke();
-            }
+            // else {
+            //     context.beginPath();
+            //     context.moveTo(startPoint.x, startPoint.y);
+            //     context.lineTo(endPoint.x * 400 + startPoint.x, endPoint.y * 400 + startPoint.y);
+            //     context.strokeStyle = '#FFF';
+            //     context.lineWidth = 0.5;
+            //     context.stroke();
+            // }
+        });
 
-        }
 
+        // if (Array.isArray(collisions)) {
+        //     context.beginPath();
+        //     context.moveTo(startPoint.x, startPoint.y);
+        //     context.lineTo(endPoint.x, endPoint.y);
+        //     if (collisions.length > 0) {
+        //         context.strokeStyle = '#fff';
+        //     } else {
+        //         context.strokeStyle = '#555';
+        //     }
+        //     context.lineWidth = 0.5;
+        //     context.stroke();
+        //
+        //     for (let i = 0; i < collisions.length; i++) {
+        //         let collision = collisions[i];
+        //         context.rect(collision.bodyA.position.x - 4.5, collision.bodyA.position.y - 4.5, 8, 8);
+        //     }
+        // }
+        // else {
+        //     if (collisions && collisions.point) {
+        //         context.beginPath();
+        //         context.moveTo(startPoint.x, startPoint.y);
+        //         context.lineTo(collisions.point.x, collisions.point.y);
+        //
+        //         // console.log('collisions: ', collisions);
+        //         context.strokeStyle = '#F88';
+        //         context.lineWidth = 0.7;
+        //         context.stroke();
+        //     }
+        //     else {
+        //         context.beginPath();
+        //         context.moveTo(startPoint.x, startPoint.y);
+        //         context.lineTo(endPoint.x * 400 + startPoint.x, endPoint.y * 400 + startPoint.y);
+        //         context.strokeStyle = '#FFF';
+        //         context.lineWidth = 0.5;
+        //         context.stroke();
+        //     }
+        // }
 
         context.fillStyle = 'rgba(255,165,0,0.7)';
         context.fill();
 
         Render.endViewTransform(render);
+        console.log("player: ", player);
     });
 
     // add mouse control
@@ -179,6 +228,7 @@ export default (width, height) => {
         engine: engine,
         runner: runner,
         render: render,
+        player: player,
         canvas: render.canvas,
         stop: () => {
             Render.stop(render);
